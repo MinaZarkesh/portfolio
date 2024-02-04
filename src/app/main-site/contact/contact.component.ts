b56111f521eb9a1e5a6e6159ab6aa73c661a7af0
Mina <mina_zarkesh@gmx.de>
2024-02-04 21:37:56 +0100

    update contact form

--- src/app/main-site/contact/contact.component.ts
+++ src/app/main-site/contact/contact.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatInputModule, MatFormFieldModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
+
 export class ContactComponent {
 
 }
