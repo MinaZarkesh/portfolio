4e29b19f694e2a3d579458287649560e84871d9b
Mina <mina_zarkesh@gmx.de>
2024-02-04 01:17:14 +0100

    update mySkills

--- src/app/main-site/skills/skills.component.ts
+++ src/app/main-site/skills/skills.component.ts
@@ -1,12 +1,62 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
+
 export class SkillsComponent {
+
+  icon = {
+    img: '',
+    text: ''
+  }
+
+  icons = [
+    {
+      img: 'assets/img/javaScript.svg',
+      text: 'JavaScript'
+    },
+    {
+      img: 'assets/img/angular.svg',
+      text: 'Angular'
+    },
+    {
+      img: 'assets/img/typescript.svg',
+      text: 'TypeScript'
+    },
+    {
+      img: 'assets/img/html.svg',
+      text: 'HTML'
+    },
+    {
+       img: 'assets/img/git-icon.svg',
+      text: 'GIT'
+    },
+    {
+       img: 'assets/img/firebase.svg',
+      text: 'Firebase'
+    },
+    {
+       img: 'assets/img/css.svg',
+      text: 'CSS'
+    },
+    {
+       img: 'assets/img/scrum.svg',
+      text: 'Scrum'
+    },
+    {
+       img: 'assets/img/api.svg',
+      text: 'Rest-Api'
+    },
+    {
+      img: 'assets/img/material-design.svg',
+      text: 'Material Design'
+    }
+  ]
 
 }
