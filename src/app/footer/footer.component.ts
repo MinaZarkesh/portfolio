07f2c8725b9400fcff2e6a9e84eea67f91539948
Mina <mina_zarkesh@gmx.de>
2024-02-11 15:04:41 +0100

    update footer responsive

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component,  } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-
+  icons = [
+    {
+      img: 'assets/img/github.png',
+      link: 'https://github.com/minazarkesh',
+    },
+    {
+      img: 'assets/img/email.png',
+      link: 'mailto:mzarkesh@gmx.de',
+    },
+    {
+      img: 'assets/img/linkedin.png',
+      link: 'https://www.linkedin.com/in/mina-zarkesh/',
+    },
+  ];
 }
