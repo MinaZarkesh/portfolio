8c910b57d829fd011ec6949f4bebfb44a9afbe13
Mina <mina_zarkesh@gmx.de>
2024-02-13 14:41:02 +0100

    update fading in animations

--- src/app/main-site/projects/project-model/project-model.component.ts
+++ src/app/main-site/projects/project-model/project-model.component.ts
@@ -1,6 +1,7 @@
 import { NgIf, NgClass, CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-
+import { OnInit } from '@angular/core';
+import  AOS from 'aos';
 @Component({
   selector: 'app-project-model',
   standalone: true,
@@ -8,10 +9,16 @@ import { Component, Input } from '@angular/core';
   templateUrl: './project-model.component.html',
   styleUrl: './project-model.component.scss'
 })
-export class ProjectModelComponent {
+export class ProjectModelComponent  implements OnInit {
   @Input() project: any; // Define the input property 'project'
   @Input() index: any; // Define the input property 'index'
   // Other component logic
+
+
+  ngOnInit(): void {
+    // Initialization logic here
+    AOS.init();
+  }
 
   toProject(link:string){
     window.open(link);
